45e926d648def04254876f93eb6bbd7c0e1b24e4
Wallace Vieira <wallaceofc@hotmail.com>
2023-12-29 12:17:47 -0300

    refactor: styles.css; Result

--- src/screens/Result/index.js
+++ src/screens/Result/index.js
@@ -1,20 +1,33 @@
+import React from 'react';
 import { Container, QuizResult } from './styles';
 import { useNavigate } from 'react-router-dom';
-
 import Button from '../../components/Button';
-
 import paroquia from '../../assets/images/icon.svg';
 import check from '../../assets/images/check-icon.svg';
 import { useQuizContext } from '../../context/QuizContext';
 
-export default function Result() {
+const Result = () => {
   const navigate = useNavigate();
   const { correctAnswer, setCorrectAnswer } = useQuizContext();
 
-  function handleFinishQuiz() {
+  const handleFinishQuiz = () => {
     setCorrectAnswer(0);
     navigate('../');
-  }
+  };
+
+  const getResultMessage = () => {
+    if (correctAnswer === 10) {
+      return 'Você foi incrível! Até o doutor angélico teria orgulho de você';
+    } else if (correctAnswer >= 8) {
+      return 'Parabéns! Você deve estudar bastante o catecismo';
+    } else if (correctAnswer >= 5) {
+      return 'Você se complicou! Seria interessante estudar um pouquinho mais o catecismo';
+    } else if (correctAnswer >= 3) {
+      return 'Ave Maria! Depois dessa é até bom se confessar';
+    } else {
+      return 'Meu irmão! Acho que precisamos falar com o padre';
+    }
+  };
 
   return (
     <Container>
@@ -27,23 +40,12 @@ export default function Result() {
           <span>{correctAnswer}0%</span>
         </QuizResult>
 
-        {correctAnswer === 10 ? (
-          <p>Você foi incrível! Até o doutor angélico teria orgulho de você</p>
-        ) : correctAnswer >= 8 ? (
-          <p>Parabéns! Você deve estudar bastante o catecismo</p>
-        ) : correctAnswer >= 5 ? (
-          <p>
-            Você se complicou! Seria interessante estudar um pouquinho mais o
-            catecismo
-          </p>
-        ) : correctAnswer >= 3 ? (
-          <p>Ave Maria! Depois dessa é até bom se confessar</p>
-        ) : (
-          <p>Meu irmão! Acho que precisamos falar com o padre</p>
-        )}
+        <p>{getResultMessage()}</p>
       </div>
 
       <Button onClick={handleFinishQuiz}>FINALIZAR</Button>
     </Container>
   );
-}
+};
+
+export default Result;
